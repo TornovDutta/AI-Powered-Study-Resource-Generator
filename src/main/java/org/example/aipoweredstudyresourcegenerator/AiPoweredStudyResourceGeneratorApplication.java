add1b3b6e54959bd0b36a1bf93296d475473288e
TornovDutta <tornovdutta@gmail.com>
2025-07-28 12:15:57 +0530

    push the basic project structure to github

---
+++ src/main/java/org/example/aipoweredstudyresourcegenerator/AiPoweredStudyResourceGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.example.aipoweredstudyresourcegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPoweredStudyResourceGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiPoweredStudyResourceGeneratorApplication.class, args);
+    }
+
+}
