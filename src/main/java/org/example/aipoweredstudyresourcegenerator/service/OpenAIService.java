14bca66d185ca8a77b2822cfd315c31023c60c0e
TornovDutta <tornovdutta@gmail.com>
2025-08-03 02:51:06 +0530

    add the application property and intergrated the LLM model

---
+++ src/main/java/org/example/aipoweredstudyresourcegenerator/service/OpenAIService.java
@@ -0,0 +1,20 @@
+package org.example.aipoweredstudyresourcegenerator.service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OpenAIService {
+
+    private final ChatClient chatClient;
+
+    public OpenAIService(OpenAiChatModel chatModel) {
+        this.chatClient = ChatClient.builder(chatModel).build();
+    }
+
+    public String getResponse(String prompt){
+        return chatClient.prompt().user(prompt).call().content().trim();
+    }
+}
