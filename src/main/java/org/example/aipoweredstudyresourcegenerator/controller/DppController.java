1f1f91fdf4fc72f41da1aab3be70dcbe9be2bb73
TornovDutta <tornovdutta@gmail.com>
2025-08-03 02:52:23 +0530

    fix the dpp controller

--- src/main/java/org/example/aipoweredstudyresourcegenerator/controller/DppController.java
+++ src/main/java/org/example/aipoweredstudyresourcegenerator/controller/DppController.java
@@ -1,16 +1,13 @@
 package org.example.aipoweredstudyresourcegenerator.controller;
-
-import org.example.aipoweredstudyresourcegenerator.service.OpenAIService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DppController {
-    @Autowired
-    private OpenAIService service;
+
+
     @GetMapping("/dpp")
     public String createDpp() {
-        return service.getResponse("hello");
+        return "";
     }
 }
