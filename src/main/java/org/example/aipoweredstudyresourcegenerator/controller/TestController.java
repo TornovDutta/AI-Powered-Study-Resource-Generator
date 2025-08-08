6dc89be1be10e30732325012805011ebd038787d
TornovDutta <tornovdutta@gmail.com>
2025-08-08 14:47:27 +0530

    fix test service

--- src/main/java/org/example/aipoweredstudyresourcegenerator/controller/TestController.java
+++ src/main/java/org/example/aipoweredstudyresourcegenerator/controller/TestController.java
@@ -2,16 +2,22 @@ package org.example.aipoweredstudyresourcegenerator.controller;
 
 
 import org.example.aipoweredstudyresourcegenerator.Model.Questions;
+import org.example.aipoweredstudyresourcegenerator.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
+    @Autowired
+    private TestService service;
     @GetMapping("create")
-    public ResponseEntity<Questions> create(@RequestBody String topic){
-        return new ResponseEntity<>(new Questions(), HttpStatus.OK);
+    public ResponseEntity<List<Questions>> create(@RequestBody String topic){
+        return new ResponseEntity<>(service.testGenerator(topic), HttpStatus.OK);
     }
 }
