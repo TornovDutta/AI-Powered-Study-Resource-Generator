7167a65b3ba6198722eae62260ed052fb115a80d
TornovDutta <tornovdutta@gmail.com>
2025-08-15 17:14:19 +0530

    fix the note service

--- src/main/java/org/example/aipoweredstudyresourcegenerator/DAO/NoteRepo.java
+++ src/main/java/org/example/aipoweredstudyresourcegenerator/DAO/NoteRepo.java
@@ -3,5 +3,10 @@ package org.example.aipoweredstudyresourcegenerator.DAO;
 import org.example.aipoweredstudyresourcegenerator.Model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NoteRepo extends JpaRepository<Note,Integer> {
+    boolean existsByTopic(String topic);
+
+    List<Note> findByTopic(String topic);
 }
