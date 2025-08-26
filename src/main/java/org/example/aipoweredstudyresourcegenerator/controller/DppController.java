ff31908adbe37886a6bfbedaba3e3fd88dce160c
TornovDutta <tornovdutta@gmail.com>
2025-08-26 15:23:34 +0530

    dpp schudle

--- src/main/java/org/example/aipoweredstudyresourcegenerator/controller/DppController.java
+++ src/main/java/org/example/aipoweredstudyresourcegenerator/controller/DppController.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -33,7 +31,15 @@ public class DppController {
     }
     @Scheduled(cron = "0 0 10 * * *")
     public ResponseEntity<List<Questions>> createscheduler(){
-        topic="java";
+        if(topic.isEmpty()){
+            return new ResponseEntity<>(new ArrayList<>(),HttpStatus.OK);
+        }
         return new ResponseEntity<>(service.dppGenerator(topic),HttpStatus.OK);
+    }
+
+    @DeleteMapping("dppStop")
+    public ResponseEntity<String> stopDpp(){
+        topic=null;
+        return  new ResponseEntity<>("ok",HttpStatus.OK);
     }
 }
