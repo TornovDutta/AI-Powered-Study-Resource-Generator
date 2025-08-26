9d2c5f9f001f9423d0e21b87d63f340d223ab571
TornovDutta <tornovdutta@gmail.com>
2025-08-26 14:49:34 +0530

    add dpp service

--- src/main/java/org/example/aipoweredstudyresourcegenerator/controller/DppController.java
+++ src/main/java/org/example/aipoweredstudyresourcegenerator/controller/DppController.java
@@ -1,14 +1,22 @@
 package org.example.aipoweredstudyresourcegenerator.controller;
+import org.example.aipoweredstudyresourcegenerator.Model.Questions;
+import org.example.aipoweredstudyresourcegenerator.service.DppService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DppController {
 
-
-    @GetMapping("/dpp")
-    public String createDpp() {
-        return "Hello";
+    @Autowired
+    private DppService service;
+    @GetMapping("create")
+    public ResponseEntity<List<Questions>> create(@RequestBody String topic){
+        return new ResponseEntity<>(service.dppGenerator(topic), HttpStatus.OK);
     }
 }
